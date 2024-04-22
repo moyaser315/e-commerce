73570a3f9c05689d73f82fbef63eb01e7d897b14
Mahmoud <mahmoud.elngar.2510@gmail.com>
2024-04-22 17:28:44 +0200

    Adding Login-Signup pages

--- Front/src/Pages/Login.jsx
+++ Front/src/Pages/Login.jsx
@@ -1,3 +1,18 @@
-const Login = () => {};
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="loginContainer">
+        <h1>Log In</h1>
+        <div className="loginFields">
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- Front/src/Pages/Signup.jsx
+++ Front/src/Pages/Signup.jsx
@@ -1,3 +1,31 @@
-const Singup = () => {};
+import "./Signup.css";
+import { Link } from "react-router-dom";
+
+const Singup = () => {
+  return (
+    <div className="signup">
+      <div className="signupContainer">
+        <h1>Sign Up</h1>
+        <div className="signupFields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Re-enter password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginDirect">
+          Already have an account ?
+          <Link className="link" to="/login">
+            <span>Login here</span>
+          </Link>
+        </p>
+        <div className="agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Singup;
