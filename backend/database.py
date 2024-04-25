e17d85eeb85fc1455fbd010f99035b28b81ee617
Mo yaser <mohamedyaser.315@gmail.com>
2024-04-26 02:09:52 +0300

    resolved conflicts

--- backend/database.py
+++ backend/database.py
@@ -13,8 +13,6 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 
 
