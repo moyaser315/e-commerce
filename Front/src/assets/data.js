259779a3d5d102e15bb0fd99a904a0bf5f56541b
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2024-04-29 06:52:16 +0300

    Add files via upload

---
+++ Front/src/assets/data.js
@@ -0,0 +1,129 @@
+import iphone_image from "../../public/iphone.jpeg"
+import jacket_image from "../../public/Clothe.jpg"
+import accessories_image from "../../public/accessories.jpg"
+
+let data_product = [
+    {
+        id: 1,
+        image: jacket_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Clothes"
+    },
+    {
+        id: 2,
+        image: iphone_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Electronics"
+    },
+    {
+        id: 3,
+        image: jacket_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Clothes"
+    },
+    {
+        id: 4,
+        image: accessories_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Accessories"
+    },
+    {
+        id: 5,
+        image: accessories_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Accessories"
+    },
+    {
+        id: 6,
+        image: iphone_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Electronics"
+    },
+    {
+        id: 7,
+        image: jacket_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Clothes"
+    },
+    {
+        id: 8,
+        image: jacket_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Clothes"
+    },
+    {
+        id: 9,
+        image: accessories_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Accessories"
+    },
+    {
+        id: 10,
+        image: iphone_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Electronics"
+    },
+    {
+        id: 11,
+        image: iphone_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Electronics"
+    },
+    {
+        id: 12,
+        image: accessories_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Accessories"
+    },
+    {
+        id: 13,
+        image: iphone_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Electronics"
+    },
+    {
+        id: 14,
+        image: jacket_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Clothes"
+    },
+    {
+        id: 15,
+        image: iphone_image,
+        name: "Name",
+        description: "Description",
+        price: 100,
+        category: "Electronics"
+    },
+  ];
+
+  export default data_product;
+  
