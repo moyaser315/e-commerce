2061f7d072d97e761280a7e35543147c56fe2862
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2024-05-01 01:59:00 +0300

    Create Footer.jsx

---
+++ Front/src/Components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import "./Footer.css";
+import footer_logo from "../../../public/R.png";
+import instagram_icon from "../../../public/instagram.jpg";
+import pinterest_icon from "../../../public/pinterest.png";
+import whatsapp_icon from "../../../public/whatsapp.jpg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-logo">
+        <img src={footer_logo} alt="" />
+        <p>Shopper</p>
+      </div>
+      <ul className="footer-links">
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icon-container">
+          <img src={instagram_icon} alt="" />
+        </div>
+        <div className="footer-icon-container">
+          <img src={pinterest_icon} alt="" />
+        </div>
+        <div className="footer-icon-container">
+          <img src={whatsapp_icon} alt="" />
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @2024 - All Rights Reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
