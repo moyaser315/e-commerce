6e98de0e400c6efad4bfa23402aeea6efc6f12e8
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2024-05-05 04:52:25 +0300

    Add files via upload

---
+++ Front/src/Pages/AddProductForm.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import "./AddProductForm.css";
+
+const AddProductForm = ({ addProduct }) => {
+  const [formData, setFormData] = useState({
+    image: "",
+    name: "",
+    description: "",
+    price: 0,
+    quantity: 0,
+    category: "Electronics & Devices",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call a function to add the product to your data store
+    addProduct(formData);
+    // Clear the form fields
+    setFormData({
+      image: "",
+      name: "",
+      description: "",
+      price: 0,
+      quantity: 0,
+      category: "Electronics & Devices",
+    });
+  };
+
+  return (
+    <form className="add-product-form" onSubmit={handleSubmit}>
+      <p>Image</p>
+      <input
+        type="text"
+        name="image"
+        value={formData.image}
+        onChange={handleChange}
+        placeholder="Image URL"
+        required
+      />
+      <p>Name</p>
+      <input
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        placeholder="Product Name"
+        required
+      />
+      <p>Description</p>
+      <textarea
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+        placeholder="Description"
+        required
+      ></textarea>
+      <p>Price</p>
+      <input
+        type="number"
+        name="price"
+        value={formData.price}
+        onChange={handleChange}
+        placeholder="Price"
+        required
+      />
+      <p>Quantity</p>
+      <input
+        type="number"
+        name="quantity"
+        value={formData.quantity}
+        onChange={handleChange}
+        placeholder="Quantity"
+        required
+      />
+      <p>Category</p>
+      <select
+        name="category"
+        value={formData.category}
+        onChange={handleChange}
+        required
+      >
+        <option value="Electronics & Devices">Electronics & Devices</option>
+        <option value="Clothes">Clothes</option>
+        <option value="Accessories">Accessories</option>
+      </select>
+      <button type="submit" className="submit-btn">
+        {/*To Do*/}
+        Submit
+      </button>
+    </form>
+  );
+};
+
+AddProductForm.propTypes = {
+  addProduct: PropTypes.func.isRequired,
+};
+
+export default AddProductForm;
--- Front/src/Pages/Profile.jsx
+++ Front/src/Pages/Profile.jsx
@@ -1,47 +1,45 @@
 import axios from "axios";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 const Profile = () => {
-    const [user, setUser] = useState(null);
-    const [products, setProducts] = useState(null);
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const token = localStorage.getItem('accessToken');
-                console.log(`Access token: ${token}`);
-                const response = await axios.get('http://localhost:8000/dashboard/',
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                if (response.data.user_info) {
-                    setUser(response.data.user_info);
-                    setProducts(response.data.products);
-                }
-                else {
-                    setUser(response.data);
-                }
-            } catch (error) {
-                console.error('Error fetching user', error);
-            }
-        };
+  const [user, setUser] = useState(null);
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem("accessToken");
+        console.log(`Access token: ${token}`);
+        const response = await axios.get("http://localhost:8000/dashboard/", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.data.user_info) {
+          setUser(response.data.user_info);
+          setProducts(response.data.products);
+        } else {
+          setUser(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching user", error);
+      }
+    };
 
-        fetchUser();
-    }, []);
+    fetchUser();
+  }, []);
 
-    if (!user){
-        return <div>Loading...</div>;
-    }
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            <p>{user.name}</p>
-            <p>This user is a {user.user_type}</p>
-            <p>{user.email}</p>
-            {user.user_type === 'seller' && <p>Sellers Product: {products}</p>}
-        </div>
-    );
+  return (
+    <div>
+      <p>{user.name}</p>
+      <p>This user is a {user.user_type}</p>
+      <p>{user.email}</p>
+      {user.user_type === "seller" && <p>Sellers Product: {products}</p>}
+    </div>
+  );
 };
 export default Profile;
