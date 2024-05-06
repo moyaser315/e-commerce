91dacf88e24edf1b378aba6fef2024b288191ecb
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2024-05-06 23:11:12 +0300

    Add files via upload

--- Front/src/Components/SellerItem/SellerItem.jsx
+++ Front/src/Components/SellerItem/SellerItem.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
-import axios from 'axios';
-import { useEffect, useState, useContext } from 'react';
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
 import "./SellerItem.css";
 import { Link } from "react-router-dom";
 
@@ -8,7 +8,7 @@ const SellerItem = (props) => {
   const handleDelete = async () => {
     /* TODO  Set quantity of product to zero and delete it from database */
     try {
-      const url = `http://localhost:8000/dashboard/${props.id}`; 
+      const url = `http://localhost:8000/dashboard/${props.id}`;
       console.log(url);
       const response = await axios.delete(url, {
         headers: {
@@ -16,52 +16,47 @@ const SellerItem = (props) => {
           Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
       });
-      
+
       if (response.status === 204) {
-        alert('Item deleted successfully!');
+        alert("Item deleted successfully!");
         window.location.reload();
-        
+      }
+    } catch (error) {
+      console.error("Error deleting item:", error);
+      alert("Failed to delete the item. Please try again.");
     }
-  } catch (error) {
-    console.error('Error deleting item:', error);
-    alert('Failed to delete the item. Please try again.');
-  }
   };
 
+  //   const handleEdit = async (updatedData) => {
+  //     try {
+  //         const url = `http://localhost:8000/dashboard/${props.id}`;
 
-//   const handleEdit = async (updatedData) => {
-//     try {
-//         const url = `http://localhost:8000/dashboard/${props.id}`; 
+  //         const response = await axios.patch(url, updatedData, {
+  //             headers: {
+  //                 "Content-Type": "application/json",
+  //                 Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  //             },
+  //         });
 
-//         const response = await axios.patch(url, updatedData, {
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//             },
-//         });
+  //         if (response.status === 200) {
+  //             alert('Item updated successfully!');
+  //             window.location.reload();
+  //         }
 
-
-//         if (response.status === 200) {
-//             alert('Item updated successfully!');
-//             window.location.reload();
-//         }
-
-//     } catch (error) {
-//         console.error('Error updating item:', error);
-//         alert('Failed to update the item. Please try again.');
-//     }
-// };
-
-
+  //     } catch (error) {
+  //         console.error('Error updating item:', error);
+  //         alert('Failed to update the item. Please try again.');
+  //     }
+  // };
 
   return (
-    <div className="card">
+    <div className="sellercard">
       <img src={props.image} alt={props.name} />
-      <div className="card-content">
+      <div className="sellercard-content">
         <h3>{props.name}</h3>
         <p className="description">{props.description}</p>
         <p>Quantity: {props.quantity}</p>
-        <div className="price-btn">
+        <div className="price">
           <span>Price: ${props.price}</span>
         </div>
         <div className="edit-delete">
