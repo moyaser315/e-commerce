9885d6e4c00e0e11e43088dd712dab9ea302759d
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2024-05-06 10:28:13 +0300

    Add files via upload

--- Front/src/Context/ShopContext.jsx
+++ Front/src/Context/ShopContext.jsx
@@ -15,22 +15,22 @@ const ShopContextProvider = (props) => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await axios.get("http://localhost:8000/", {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            setProducts(response.data);
-            setLoading(false);
-            console.log(response.data);
-        } catch (error) {
-            console.error("Error fetching products", error);
-        }
+      try {
+        const response = await axios.get("http://localhost:8000/", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        setProducts(response.data);
+        setLoading(false);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching products", error);
+      }
     };
 
     fetchData();
-}, []);
+  }, []);
 
   console.log(products);
   const [cartItems, setCartItems] = useState(getDefaultCart());
@@ -38,66 +38,61 @@ const ShopContextProvider = (props) => {
   const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
     const updateCartFromBackend = async () => {
-        try {
-          const cartItemsFromBackEnd = await axios.get(`${hostname}/cart`, {
-            headers: { 'Content-Type': 'application/json' ,
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+      try {
+        const cartItemsFromBackEnd = await axios.get(`${hostname}/cart`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
           },
-          });
+        });
 
-          setCartItems(cartItemsFromBackEnd.data);
+        setCartItems(cartItemsFromBackEnd.data);
 
-          // console.log("response", response.data);
-          console.log("backend", cartItemsFromBackEnd.data);
-          setErrorMessage("")
-          setEdit(false);
-        } catch (error) {
-          console.error('Failed to update cart in backend:', error);
-          setEdit(false);
-        }
+        // console.log("response", response.data);
+        console.log("backend", cartItemsFromBackEnd.data);
+        setErrorMessage("");
+        setEdit(false);
+      } catch (error) {
+        console.error("Failed to update cart in backend:", error);
+        setEdit(false);
+      }
     };
-  
-    updateCartFromBackend();
-  }, []); 
 
+    updateCartFromBackend();
+  }, []);
 
   const addToCart = async (itemId) => {
     try {
       // Get the current state of the cart
-      const cartResponse = await axios.get(
-        `${hostname}/cart`,
-        {
-          headers: {
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
-          }
-        }
-      );
-  
+      const cartResponse = await axios.get(`${hostname}/cart`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
       // Find the item in the cart
-      const existingItem = cartResponse.data.find((item) => item.productID === itemId);
-  
+      const existingItem = cartResponse.data.find(
+        (item) => item.productID === itemId
+      );
+
       // If the item exists, increment its quantity, otherwise set it to 1
       const updatedQuantity = existingItem ? existingItem.quantity + 1 : 1;
-  
-      const updatedItem = { "productID": itemId, "quantity": updatedQuantity };
-  
+
+      const updatedItem = { productID: itemId, quantity: updatedQuantity };
+
       // Send the updated item back to the backend
-      const response = await axios.put(
-        `${hostname}/cart`,
-        updatedItem,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
-          }
-        }
-      );
-  
+      const response = await axios.put(`${hostname}/cart`, updatedItem, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
       console.log("response", response.data);
       setEdit(true);
     } catch (error) {
       console.error(error);
-      setErrorMessage(error.response.data.detail)
+      setErrorMessage(error.response.data.detail);
     }
   };
 
@@ -105,80 +100,72 @@ const ShopContextProvider = (props) => {
   const removeFromCart = async (itemId) => {
     try {
       // Get the current state of the cart
-      const cartResponse = await axios.get(
-        `${hostname}/cart`,
-        {
-          headers: {
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
-          }
-        }
-      );
-  
+      const cartResponse = await axios.get(`${hostname}/cart`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
       // Find the item in the cart
-      const existingItem = cartResponse.data.find((item) => item.productID === itemId);
-  
+      const existingItem = cartResponse.data.find(
+        (item) => item.productID === itemId
+      );
+
       if (!existingItem) {
         // If the item doesn't exist in the cart, there's nothing to remove
         return;
       }
-  
+
       // Decrement the item's quantity or set it to 0 if it's already 1
-      existingItem.quantity = existingItem.quantity > 1 ? existingItem.quantity - 1 : 0;
-  
+      existingItem.quantity =
+        existingItem.quantity > 1 ? existingItem.quantity - 1 : 0;
+
       // Send the updated item back to the backend
-      const response = await axios.put(
-        `${hostname}/cart`,
-        existingItem,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
-          }
-        }
-      );
-  
+      const response = await axios.put(`${hostname}/cart`, existingItem, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
       console.log("response", response.data);
       setEdit(true);
     } catch (error) {
       console.error(error);
-      setErrorMessage(error.response.data.detail)
+      setErrorMessage(error.response.data.detail);
     }
   };
   /*To Test*/
   const clearCart = async () => {
     try {
       // Get the current state of the cart
-      const cartResponse = await axios.get(
-        `${hostname}/cart`,
-        {
-          headers: {
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
-          }
-        }
-      );
-  
+      const cartResponse = await axios.get(`${hostname}/cart`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
       // Set the quantity of each item to 0
-      const updatedItems = cartResponse.data.map((item) => ({ ...item, quantity: 0 }));
-  
+      const updatedItems = cartResponse.data.map((item) => ({
+        ...item,
+        quantity: 0,
+      }));
+
       // Send the updated items back to the backend
       for (const item of updatedItems) {
-        await axios.put(
-          `${hostname}/cart`,
-          item,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
-            }
-          }
-        );
+        await axios.put(`${hostname}/cart`, item, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
       }
-  
+
       console.log("Cart cleared");
       setEdit(true);
     } catch (error) {
       console.error(error);
-      setErrorMessage(error.response.data.detail)
+      setErrorMessage(error.response.data.detail);
     }
   };
 
@@ -207,26 +194,27 @@ const ShopContextProvider = (props) => {
       if (edit) {
         try {
           const cartItemsFromBackEnd = await axios.get(`${hostname}/cart`, {
-            headers: { 'Content-Type': 'application/json' ,
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
-          },
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
           });
 
           setCartItems(cartItemsFromBackEnd.data);
 
           // console.log("response", response.data);
           console.log("backend", cartItemsFromBackEnd.data);
-          setErrorMessage("")
+          setErrorMessage("");
           setEdit(false);
         } catch (error) {
-          console.error('Failed to update cart in backend:', error);
+          console.error("Failed to update cart in backend:", error);
           setEdit(false);
         }
       }
     };
-  
+
     updateCartFromBackend();
-  }, [edit]); 
+  }, [edit]);
 
   const contextValue = {
     getTotalCartItems,
@@ -235,7 +223,8 @@ const ShopContextProvider = (props) => {
     addToCart,
     removeFromCart,
     clearCart,
-    errorMessage
+    products,
+    errorMessage,
   };
 
   return (
