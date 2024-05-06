e1b9d8b4e31b39c10ebbf4b61814117587418044
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2024-05-06 06:36:29 +0300

    Add files via upload

--- Front/src/Components/SellerItem/SellerItem.jsx
+++ Front/src/Components/SellerItem/SellerItem.jsx
@@ -1,16 +1,29 @@
 import PropTypes from "prop-types";
 import "./SellerItem.css";
+import { Link } from "react-router-dom";
 
 const SellerItem = (props) => {
+  const handleDelete = () => {
+    /* TODO  Set quantity of product to zero and delete it from database */
+  };
+
   return (
-    <div className="row">
-      <img src={props.image} alt={props.name} /> <hr />
-      <div className="row-content">
+    <div className="card">
+      <img src={props.image} alt={props.name} />
+      <div className="card-content">
         <h3>{props.name}</h3>
-        <p>{props.description}</p>
+        <p className="description">{props.description}</p>
         <p>Quantity: {props.quantity}</p>
         <div className="price-btn">
           <span>Price: ${props.price}</span>
+        </div>
+        <div className="edit-delete">
+          <Link to={`/edit/${props.id}`}>
+            <button className="edit">Edit</button>
+          </Link>
+          <button className="delete" onClick={handleDelete}>
+            Delete
+          </button>
         </div>
       </div>
     </div>
