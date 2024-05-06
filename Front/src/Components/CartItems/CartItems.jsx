64a56d301f0f4b0b2cd432ef8f24379a54395e25
Ali Omran <aliomran918273@gmail.com>
2024-05-07 00:59:08 +0300

    CartItems.css

--- Front/src/Components/CartItems/CartItems.jsx
+++ Front/src/Components/CartItems/CartItems.jsx
@@ -1,72 +1,94 @@
 import "./CartItems.css";
-import { useEffect, useState, Link, useContext } from 'react';
-import axios from 'axios';
-import remove_icon from '../../../public/removeicon.png';
+import { useEffect, useState, Link, useContext } from "react";
+import axios from "axios";
+import remove_icon from "../../../public/removeicon.png";
 import { hostname } from "../../assets/globalVars";
-import { ShopContext } from '../../Context/ShopContext';
+import { ShopContext } from "../../Context/ShopContext";
 import { ProductContext } from "../../Context/ProductContext";
-
 
 const CartItems = () => {
   const {
     getTotalCartAmount,
     cartItems,
-    removeFromCart, 
+    removeFromCart,
     clearCart,
-    addToCart
+    addToCart,
   } = useContext(ShopContext);
-const {products} = useContext(ProductContext);
-const [orderDetails, setOrderDetails] = useState(null);
-console.log(products);
-const handleCheckout = async () => {
-  try {
-      const response = await axios.get('http://localhost:8000/checkout', {
+  const { products } = useContext(ProductContext);
+  const [orderDetails, setOrderDetails] = useState(null);
+  console.log(products);
+  const handleCheckout = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/checkout", {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
       });
-      
+
       setOrderDetails(response.data);
-      alert('Your order has been completed successfully');
-      
+      alert("Your order has been completed successfully");
+
       clearCart();
-  } catch (error) {
+    } catch (error) {
       // Handle errors
-      console.error('Error during checkout:', error);
-      alert('Checkout failed. Please try again.');
-  }
-};
+      console.error("Error during checkout:", error);
+      alert("Checkout failed. Please try again.");
+    }
+  };
   return (
-      <div className="cartitems">
-        <div className="cartitems-format-main">
-          <p>Product</p>
-          <p>Name</p>
-          <p>Price</p>
-          <p>Quantity</p>
-          <p>Total</p>
-          <p>Add/Remove</p>
-        </div>
-        <hr />
-          {Array.isArray(products) && cartItems.map((cartItem) => {
-            const product = products.find((product) => product.id === cartItem.productID);
-            console.log("Prodict ID:", cartItem.productID, "Matched product", product);
-            return (
-              <div key={cartItem.productID}>
-                <div className="cartitems-format cartitems-format-main">
-                  <img src={product.imgPath} alt="" className="carticon-product-icon" />
-                  <p>{product.name}</p>
-                  <p>${product.price}</p>
-                  <p>{cartItem.quantity}</p>
-                  <p>{product.price * cartItem.quantity}</p>
-                  <div className="cartitems-format button-container">
-                    <button onClick={() => addToCart(cartItem.productID)}>+</button>
-                    <button onClick={() => removeFromCart(cartItem.productID)}>-</button>
-                  </div>
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Product</p>
+        <p>Name</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove/Add</p>
+      </div>
+      <hr />
+      {Array.isArray(products) &&
+        cartItems.map((cartItem) => {
+          const product = products.find(
+            (product) => product.id === cartItem.productID
+          );
+          console.log(
+            "Prodict ID:",
+            cartItem.productID,
+            "Matched product",
+            product
+          );
+          return (
+            <div key={cartItem.productID}>
+              <div className="cartitems-format cartitems-format-main">
+                <img
+                  src={product.imgPath}
+                  alt=""
+                  className="carticon-product-icon"
+                />
+                <p>{product.name}</p>
+                <p>${product.price}</p>
+                <p>{cartItem.quantity}</p>
+                <p>{product.price * cartItem.quantity}</p>
+                <div className="cartitems-format button-container">
+                  <button
+                    className="remove"
+                    onClick={() => removeFromCart(cartItem.productID)}
+                  >
+                    -
+                  </button>
+                  <button
+                    className="add"
+                    onClick={() => addToCart(cartItem.productID)}
+                  >
+                    +
+                  </button>
                 </div>
               </div>
-            );})}
-          <hr />
+            </div>
+          );
+        })}
+      <hr />
       <div className="cartitems-down">
         <div className="cartitems-total">
           <h1>Cart Total</h1>
@@ -94,21 +116,16 @@ const handleCheckout = async () => {
             <input type="text" placeholder="Address" />
             <input type="text" placeholder="Credit Card Number" />
             <hr />
-            {/* TODO: handle checkout (backend)*/}
-              <button onClick={handleCheckout} >
-                Checkout
-              </button>
+            <button onClick={handleCheckout}>Checkout</button>
           </div>
         </div>
       </div>
       {orderDetails && (
-    <div className="order-details">
-        <h3>Order Details:</h3>
-        <pre>{JSON.stringify(orderDetails, null, 2)}</pre>
-    </div>
-)}
-
-
+        <div className="order-details">
+          <h3>Order Details:</h3>
+          <pre>{JSON.stringify(orderDetails, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 };
