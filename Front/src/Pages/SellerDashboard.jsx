ec1a4909555e1bcc8a6699e2f401abaff01c0dc0
Mahmoud <mahmoud.elngar.2510@gmail.com>
2024-05-07 20:01:39 +0300

    Seller sold products

--- Front/src/Pages/SellerDashboard.jsx
+++ Front/src/Pages/SellerDashboard.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import AddProductForm from "./AddProductForm.jsx";
 import "./SellerDashboard.css";
 import SellerItem from "../Components/SellerItem/SellerItem.jsx";
+import SellerItem2 from "../Components/SellerItem/SellerSoldItem2.jsx";
 import axios from "axios";
 
 const SellerDashboard = () => {
@@ -53,6 +54,22 @@ const SellerDashboard = () => {
         {Array.isArray(products) &&
           products.map((item, i) => (
             <SellerItem
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.imgPath}
+              description={item.description}
+              quantity={item.quantity}
+              price={item.price}
+              category={item.category}
+            />
+          ))}
+      </div>
+      <h1>Sold Products:</h1>
+      <div className="product-cards">
+        {Array.isArray(products) &&
+          products.map((item, i) => (
+            <SellerItem2
               key={i}
               id={item.id}
               name={item.name}
