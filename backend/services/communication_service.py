8e0e2c6af0f2c5451bbf9a36f9badb837db6b968
Mohamed Yaser <mohamedyaser.315@gmail.com>
2025-07-26 16:25:19 +0300

    structure comm

---
+++ backend/services/communication_service.py
@@ -0,0 +1,26 @@
+#TODO: Implement Twilio methods
+class CommunicationService:
+    @staticmethod
+    def twilio_wp():
+        pass
+    
+    @staticmethod
+    def twilio_sms():
+        pass
+    
+    @staticmethod
+    def twilio_mail():
+        pass
+    
+    @staticmethod
+    def twilio_call():
+        pass
+    
+    @staticmethod
+    def twilio_video():
+        pass
+    
+    @staticmethod
+    def twilio_flex():
+        pass
+    
